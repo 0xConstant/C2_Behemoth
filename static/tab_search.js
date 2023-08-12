d901f0cdc575b5f038b64135275de51a4e0ef83d
0xkasra <noroh4xy@protonmail.com>
2023-08-11 22:49:16 -0400

    search tabs work fine

---
+++ static/tab_search.js
@@ -0,0 +1,22 @@
+
+    $(document).ready(function(){
+
+    // For the first table
+    $("#search-input").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+
+        $("#users-table tr").filter(function() {
+            $(this).toggle($(this).find('.uid-cell').text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
+    // For the second table
+    $("#search-paid-input").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+
+        $("#users-paid-table tr").filter(function() {
+            $(this).toggle($(this).find('.uid-cell').text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+
